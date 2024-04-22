46126e3f2a93a6cb61e8946c4df8ca8a3f5f12f0
bedolaga <tnikita912@gmail.com>
2024-04-22 21:37:15 +0700

    1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,155 +1,128 @@
-#include <iostream>
-#include <cmath>
-#include <string>
-#include <cctype>
-using namespace std;
-// Функция для проверки простоты числа
-bool is_prime(int p) {
-    if (p <= 1) return false;
-    for (int i = 2; i <= sqrt(p); i++) {
-        if (p % i == 0) return false;
-    }
-    return true;
-}
-
-int NOD (int a, int b) {
-    if (b == 0) {
-        return a;
-    }
-    return NOD(b , a % b);
-}
-
-bool is_Eiler(int a, int x, int p) {
-    if (NOD(a,p) == 1) {
-        return true;
-    }
-    return false;
-}
-
-int Eiler(int p) {
-    int result = p;
-    for (int i = 2; i * i <= p; i++) {
-        if (p % i == 0) {
-            while (p % i == 0)
-                p /= i;
-            result -= result / i;
-        }
-    }
-    if (p > 1)
-        result -= result / p;
-    return result;
-}
-
-bool is_Int (string n) {
-    if (n[0] == '-') {
-        n[0] = '1';
-    }
-    for (char c : n) {
-        if (!isdigit(c)) {
-            cout << "Некорректные данные" << endl;
-            return false;
-        }
-    }
-    return true;
-}
-
-// Функция для вычисления a^x mod p используя метод двоичного разложения степени
-int pow_mod(int a, int x, int p) {
-    int result = 1;
-    a = a % p; // Уменьшаем a по модулю p
-    while (x > 0) {
-        if (x % 2 == 1) {
-            result = (result * a) % p;
-        }
-        a = (a * a) % p;
-        x /= 2;
-    }
-    return result;
-}
-
-int main() {
-    string astr, xstr, pstr, bstr, ystr;
-    int a, x, p, b, y;
+#include <iostream>  
+#include <cmath>  
+#include <vector>  
+  
+using namespace std;  
+  
+int NOD (int a, int b) {  
+    if (b == 0) {  
+        return a;  
+    }  
+    return NOD(b , a % b);  
+}  
+  
+bool Prime(int a) {  
+    if (a == 1)  return false;  
+    for (int i = 2; i < sqrt(a); i++) {  
+        if (a % i == 0) return false;  
+    }  
+    return true;  
+}  
+  
+int EilerFunc(int p) { // определяем количество взаимно простых чисел для p   
+    int result = p;  
+    for (int i = 2; i <= sqrt(p); i++) { //проходимся до корня из числа  
+        //если число разделилось на i, то мы уменьшаем р на i так как все числа,  
+        while (p % i == 0) { // которые делятся на i не являются взаимно простыми с p  
+            p /= i;  
+        }  
+        result -= result / i; // из результата вычитаем количество чисел result/i  
+    }  
+    if (p > 1) { // если п - простое число, которое не разделилось, то оно простое и результатом  
+        result -= result / p; // функции будет р-1  
+    }  
+  
+    return result;  
+}  
+  
+int modexp(int basis, int power, int modP) {  
+    if (power == 0) {  
+        return 1;  
+    }  
+    int z = modexp(basis % modP, power / 2, modP) % modP;  
+    if (power % 2 == 0) {  
+        return (z * z) % modP;  
+    }  
+    else {  
+        return ((basis % modP) * ((z * z) % modP)) % modP;  
+    }  
+}  
+  
+pair<int, int> remainder(int& basis, int& power, int& modP) {  
+    pair<int, int> ost = { 1,1 };  
+  
+    ost.first = modexp(basis, power, modP);  
+  
+    int logByNumber = log2(power);     //рассматриваем остатки через логарифм и 2сс.  
+    vector<int> binary;  //переводим степень в 2сс.  
+    int power2 = power;  
+    while (power2 > 0) {  
+        binary.push_back(power2 % 2);  
+        power2 /= 2;  
+    }  
+  
+    int resultForPrime = 1;  
+    for (int i = 0; i <= logByNumber; i++) {  
+        if (binary[i] == 1) { //бинарное не переворачиваем, а просто идем с "конца"  
+            int oneDegree = pow(2, i);  
+            resultForPrime *= modexp(basis, oneDegree, modP);  
+        }  
+    }  
+    ost.second = resultForPrime % modP;  //после смотрим на остаток  
+  
+    return ost;  //вернем пару значений  
+} 
+  
+int main() { 
     
-    cout << "Введите a: ";
-    cin >> astr;
-    if (!is_Int(astr))  {
-        return 1;
-    };
-    a = stoi(astr);
-    cout << "Введите x: ";
-    cin >> xstr;
-    if (!is_Int(xstr))  {
-        return 1;
-    };
-    x = stoi(xstr);
-    cout << "Введите b: ";
-    cin >> bstr;
-    if (!is_Int(bstr))  {
-        return 1;
-    };
-    b = stoi(bstr);
-    cout << "Введите y: ";
-    cin >> ystr;
-    if (!is_Int(ystr))  {
-        return 1;
-    };
-    y = stoi(ystr);
-    cout << "Введите p: ";
-    cin >> pstr;
-    if (!is_Int(pstr))  {
-        return 1;
-    };
-    p = stoi(pstr);
-    // Проверяем, простое ли число p
-    if (!is_prime(p)) {
-        if (!is_Eiler(a,x,p)) {
-            cout << "Для" << a << "^" << x << " mod " << p << " теорема Эйлера не выполняется" << endl;
-        }
-        if (!is_Eiler(b,y,p)) {
-            cout << "Для" << b << "^" << y << " mod " << p << " теорема Эйлера не выполняется" << endl;
-        }
-        if (is_Eiler(a,x,p) && is_Eiler(b,y,p)) {
-            if (pow_mod(a,x%Eiler(p),p)==pow_mod(b,y%Eiler(p),p)) {
-                cout << Eiler(p) << endl;
-                cout << " (" << a << "^" << x << " mod " << p << " = " << pow_mod(a,x%Eiler(p),p) << ") = (" << b << "^" << y << " mod " << p << " = " << pow_mod(b,y%Eiler(p),p) << ")" << endl;
-            }
-            else {
-                cout << " (" << a << "^" << x << " mod " << p << " = " << pow_mod(a,x%Eiler(p),p) << ") != (" << b << "^" << y << " mod " << p << " = " << pow_mod(b,y%Eiler(p),p) << ")" << endl;
-            }
-        }
-        else {
-            if (pow_mod(a,x,p)==pow_mod(b,y,p)) {
-                cout << " (" << a << "^" << x << " mod " << p << " = " << pow_mod(a,x,p) << ") = (" << b << "^" << y << " mod " << p << " = " << pow_mod(b,y,p) << ")" << endl;
-            }
-            else {
-                cout << " (" << a << "^" << x << " mod " << p << " = " << pow_mod(a,x,p) << ") != (" << b << "^" << y << " mod " << p << " = " << pow_mod(b,y,p) << ")" << endl;
-            }
-        }
+    int basis = 0, basis2 = 0, power = 0, power2 = 0, modP = 0; 
+ 
+    cout << "Введите основание числа: "; 
+    cin >> basis; 
+    if (cin.fail()) { 
+        cout << "Ошибка: введено не число." << endl; 
+        return 1; 
     } 
-    else {
-        if (!is_Eiler(a,x,p)) {
-            cout << "Для" << a << "^" << x << " mod " << p << " теорема Ферма не выполняется" << endl;
-        }
-        if (!is_Eiler(b,y,p)) {
-            cout << "Для" << b << "^" << y << " mod " << p << " теорема Ферма не выполняется" << endl;
-        }
-        if (is_Eiler(a,x,p) && is_Eiler(b,y,p)) {
-            if (pow_mod(a,x%(p-1),p)==pow_mod(b,y%(p-1),p)) {
-                cout << " (" << a << "^" << x << " mod " << p << " = " << pow_mod(a,x%(p-1),p) << ") = (" << b << "^" << y << " mod " << p << " = " << pow_mod(b,y%(p-1),p) << ")" << endl;
-            }
-            else {
-                cout << " (" << a << "^" << x << " mod " << p << " = " << pow_mod(a,x%(p-1),p) << ") != (" << b << "^" << y << " mod " << p << " = " << pow_mod(b,y%(p-1),p) << ")" << endl;
-            }
-        }
-        else {
-            if (pow_mod(a,x,p)==pow_mod(b,y,p)) {
-                cout << " (" << a << "^" << x << " mod " << p << " = " << pow_mod(a,x,p) << ") = (" << b << "^" << y << " mod " << p << " = " << pow_mod(b,y,p) << ")" << endl;
-            }
-            else {
-                cout << " (" << a << "^" << x << " mod " << p << " = " << pow_mod(a,x,p) << ") != (" << b << "^" << y << " mod " << p << " = " << pow_mod(b,y,p) << ")" << endl;
-            }
-        }
-    }
-    return 0;
+ 
+    cout << "Введите степень числа: "; 
+    cin >> power; 
+    if (cin.fail()) { 
+        cout << "Ошибка: введено не число." << endl; 
+        return 1; 
+    } 
+ 
+    cout << "Введите основание второго числа: "; 
+    cin >> basis2; 
+    if (cin.fail()) { 
+        cout << "Ошибка: введено не число." << endl; 
+        return 1; 
+    } 
+ 
+    cout << "Введите степень числа второго числа: "; 
+    cin >> power2; 
+    if (cin.fail()) { 
+        cout << "Ошибка: введено не число." << endl; 
+        return 1; 
+    } 
+ 
+    cout << "Введите модуль числа: "; 
+    cin >> modP; 
+    if (cin.fail()) { 
+        cout << "Ошибка: введено не число." << endl; 
+        return 1; 
+    } 
+    
+    int result1 = modexp(basis, power, modP); 
+    cout << "1-ый результат: " << result1 << endl; 
+    int result2 = modexp(basis2, power2, modP); 
+    cout << "2-ой результат: " << result2 << endl;  
+ 
+    if (result1 > result2) { 
+        cout << basis << "^" << power << " mod " << modP << " > " << basis2 << "^" << power2 << " mod " << modP << endl; 
+    } else if (result1 < result2) { 
+        cout << basis << "^" << power << " mod " << modP << " < " << basis2 << "^" << power2 << " mod " << modP << endl; 
+    } else if (result1 = result2) { 
+        cout << basis << "^" << power << " mod " << modP << " = " << basis2 << "^" << power2 << " mod " << modP << endl; 
+    } 
+    return 0; 
 }
